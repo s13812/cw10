44c940e04394ce74e88838ccabcf1d25f62b163b
Michal Razowski <s13812@pjwstk.edu.pl>
2021-01-29 12:02:58 +0000

    cw10: zad 1

---
+++ cw10/Models/cw10Context.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace cw10.Models
+{
+    public partial class Cw10Context : DbContext
+    {
+        public Cw10Context()
+        {
+        }
+
+        public Cw10Context(DbContextOptions<Cw10Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Enrollment> Enrollments { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Study> Studies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=cw10;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Enrollment>(entity =>
+            {
+                entity.HasKey(e => e.IdEnrollment)
+                    .HasName("Enrollment_pk");
+
+                entity.ToTable("Enrollment");
+
+                entity.Property(e => e.IdEnrollment).ValueGeneratedNever();
+
+                entity.Property(e => e.StartDate).HasColumnType("date");
+
+                entity.HasOne(d => d.IdStudyNavigation)
+                    .WithMany(p => p.Enrollments)
+                    .HasForeignKey(d => d.IdStudy)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Enrollment_Study");
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.IndexNumber)
+                    .HasName("Student_pk");
+
+                entity.ToTable("Student");
+
+                entity.Property(e => e.IndexNumber).HasMaxLength(100);
+
+                entity.Property(e => e.BirthDate).HasColumnType("date");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.HasOne(d => d.IdEnrollmentNavigation)
+                    .WithMany(p => p.Students)
+                    .HasForeignKey(d => d.IdEnrollment)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("Student_Enrollment");
+            });
+
+            modelBuilder.Entity<Study>(entity =>
+            {
+                entity.HasKey(e => e.IdStudy)
+                    .HasName("Study_pk");
+
+                entity.ToTable("Study");
+
+                entity.Property(e => e.IdStudy).ValueGeneratedNever();
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
